6768efa993fb122e9edd052ac6732bedda3f9c6d
tahid02 <tanzim1463@gmail.com>
2022-04-02 17:24:34 +0600

    send message in chat

--- router/inboxRouter.js
+++ router/inboxRouter.js
@@ -7,9 +7,11 @@ const {
   getInbox,
   searchUser,
   getMessages,
+  sendMessage,
 } = require("../controller/inboxController");
 const { checkLogin } = require("../middlewares/common/checkLogin");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const attachmentUpload = require("../middlewares/inbox/attachmentUpload");
 
 const router = express.Router();
 
@@ -22,5 +24,7 @@ router.post("/search", checkLogin, searchUser);
 
 // get messages of a conversation
 router.get("/messages/:conversation_id", checkLogin, getMessages);
+// send message
+router.post("/message", checkLogin, attachmentUpload, sendMessage);
 
 module.exports = router;
